d18d62bec69432ae35b48b1fbb33871baca5efb4
Daniel <dcc.maugeri@gmail.com>
2012-10-17 22:53:07 -0400

    added comments to show what puzzle the program is solving

--- puzzle.pl
+++ puzzle.pl
@@ -1,4 +1,16 @@
 /*Generate and Test*/
+/*Solves the problem 
+      S A Y
+    *   M Y
+    --------
+    N A M E
++ A M N E
+------------
+  S T Y L E
+
+where each letter stands for a distinct digit and that leading digits are not zeroes using pure generate and test technique
+use the query solve(L) to get each value in the list */
+
 dig(0). dig(1). dig(2). dig(3). dig(4). dig(5). dig(6).
 dig(7). dig(8). dig(9).
 
