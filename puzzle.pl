ee3d4b0ce4594b9896f1234fb46de5af9cc825fd
Daniel <dcc.maugeri@gmail.com>
2012-10-18 21:13:15 -0400

    fixed bug, and added print_solution

--- puzzle.pl
+++ puzzle.pl
@@ -15,6 +15,16 @@ use the query solve(L) to get each value in the list */
 dig(0). dig(1). dig(2). dig(3). dig(4). dig(5). dig(6).
 dig(7). dig(8). dig(9).
 
+print_solution :-
+    solve([S,A,Y,M,N,E,T,L]),
+    write('    '), write(S), write(A), write(Y), nl,
+    write('  *  '), write(M), write(Y), nl,
+    write('  ------'), nl, 
+    write('   '), write(N), write(A), write(M), write(E), nl,
+    write('+ '), write(A), write(M), write(N), write(E), write(' '), nl,
+    write('--------'),nl,
+    write('  '), write(S), write(T), write(Y), write(L), write(E).
+
 solve([S,A,Y,M,N,E,T,L]):-
 	/*checking domain*/
 	dig(S), dig(A), dig(Y), dig(M), dig(N), dig(E), dig(T), dig(L),
@@ -28,11 +38,6 @@ solve([S,A,Y,M,N,E,T,L]):-
 	M is (Y*A+C1) mod 10, C2 is (Y*A+C1) // 10,
 	A is (Y*S+C2) mod 10, N is (Y*S+C2) // 10,
 
-	/*multiplying the M digit*/
-	E is (M*Y) mod 10, C3 is (M*Y) // 10,
-	N is (M*A+C3) mod 10, C4 is (M*A + C3) // 10,
-	M is (M*S+C4) mod 10, A is (M*S+C4) // 10,
-
 	/*adding digits NAME and AMNE*/
 	L is (M+E) mod 10, C5 is (M+E) // 10,
 	Y is (A+N+C5) mod 10, C6 is (A+N+C5) // 10,
@@ -45,5 +50,5 @@ all_diff([]).
 all_diff([H|T]):-not memberA(H,T), all_diff(T).
 
 /*check if member is within a list*/
-memberA(N,[H|T]).
+memberA(H,[H|T]).
 memberA(N,[H|T]):-memberA(N,T).
