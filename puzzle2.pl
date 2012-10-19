169629234e33dc5299f3159c0950614913e487aa
Daniel <dcc.maugeri@gmail.com>
2012-10-18 21:16:15 -0400

    started interleaving puzzle

---
+++ puzzle2.pl
@@ -0,0 +1,17 @@
+/*Interleaving*/
+/*Solves the problem 
+      S A Y
+    *   M Y
+    --------
+    N A M E
++ A M N E
+------------
+  S T Y L E
+
+where each letter stands for a distinct digit and that leading digits are not zeroes using interleaving technique 
+use the query solve(L) to get each value in the list */
+
+/* distinct digits 0-9 is our domain */
+dig(0). dig(1). dig(2). dig(3). dig(4). dig(5). dig(6).
+dig(7). dig(8). dig(9).
+
