75f1028d26e5804c097676b04892c05639e74fbf
Daniel <dcc.maugeri@gmail.com>
2012-10-20 15:18:39 -0400

    finished schedule program

--- schedule.pl
+++ schedule.pl
@@ -1,8 +1,8 @@
-/*Program should choose all days for all the courses the days are December 11,
+/*Program should choose all days for all the course exams the days are December 11,
 12, 13 and December 14. Courses are:
 
 Cognitive Science: csc199, mat120, mat140;
-Artificial Intelligence: csc199, csc108, mat140;
+Artificial Intelligence: csc199, csc108, csc148, mat140;
 Philosophy of Mind: csc199, mat101, phl250
 
 The constraints are:
@@ -17,7 +17,20 @@ day(11). day(12). day(13). day(14).
 
 %each variable represents when the exam for the course will be taken
 
-solve([E199,E120,E140,E108,E101,E250,E148]):-
+solve([CSC199,MAT120,MAT140,CSC148,CSC108,MAT101,PHL250]):-
+    day(CSC199), day(MAT120), day(MAT140), day(CSC148), day(CSC108), day(MAT101), day(PHL250),
+    PHL250 = 11, not MAT140 = 14,
+    all_diff([CSC199,MAT120,MAT140]),
+    all_diff([CSC199,CSC108,CSC148,MAT140]),
+    all_diff([CSC199,MAT101,PHL250]),
+    CSC199 >= CSC148, CSC148 >= CSC108,
+    MAT140 >= MAT120, MAT120 >= MAT101.
 
+memberOf(N,[N|T]).
+memberOf(N,[H|T]):-
+    memberOf(N,T).
 
-sameDay(E) :- 
+all_diff([]).
+all_diff([H|T]):-
+    not memberOf(H,T), all_diff(T).
+
